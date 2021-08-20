637ee780c93063869aacf02bf20b65a5c483ca27
rqcruz <ronaldocruz183@gmail.com>
2021-08-19 22:07:42 -0300

    Criado ola mundo para teste inicial

---
+++ src/test/java/br/com/rcruz/rest/OlaMundo.java
@@ -0,0 +1,12 @@
+package br.com.rcruz.rest;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+
+public class OlaMundo {
+    public static void main(String[] args) {
+        Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola");
+        System.out.println(response.getBody().asString());
+    }
+}
