ebbd55ed64257685d5b2aff55817fce91bd0aced
rqcruz <ronaldocruz183@gmail.com>
2021-08-19 22:29:04 -0300

    Efetuando request por status code

--- src/test/java/br/com/rcruz/rest/OlaMundo.java
+++ src/test/java/br/com/rcruz/rest/OlaMundo.java
@@ -8,5 +8,6 @@ public class OlaMundo {
     public static void main(String[] args) {
         Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola");
         System.out.println(response.getBody().asString());
+        System.out.println(response.getStatusCode());
     }
 }
