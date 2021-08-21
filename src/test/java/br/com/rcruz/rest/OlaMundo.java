ab9c79d77e8132e57e7f13285400880a7171af95
rqcruz <ronaldocruz183@gmail.com>
2021-08-20 21:34:10 -0300

    Usando apenas ValidateResponse do rest assured

--- src/test/java/br/com/rcruz/rest/OlaMundo.java
+++ src/test/java/br/com/rcruz/rest/OlaMundo.java
@@ -3,11 +3,16 @@ package br.com.rcruz.rest;
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 
 public class OlaMundo {
     public static void main(String[] args) {
         Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola");
-        System.out.println(response.getBody().asString());
-        System.out.println(response.getStatusCode());
+        System.out.println(response.getBody().asString().equals("Ola"));
+        System.out.println(response.getStatusCode() == 201);
+
+        ValidatableResponse validate = response.then();
+
+        validate.statusCode(201);
     }
 }
