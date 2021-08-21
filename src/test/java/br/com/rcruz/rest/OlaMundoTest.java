df28a67fcf74869f313d102924cd666bbe843d58
rqcruz <ronaldocruz183@gmail.com>
2021-08-20 22:27:45 -0300

    Teste duplicado e menos verboso

--- src/test/java/br/com/rcruz/rest/OlaMundoTest.java
+++ src/test/java/br/com/rcruz/rest/OlaMundoTest.java
@@ -1,5 +1,7 @@
 package br.com.rcruz.rest;
 
+import static io.restassured.RestAssured.*;
+
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
@@ -11,13 +13,22 @@ public class OlaMundoTest {
 
     @Test
     public void testOlaMundo() {
-        Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola");
+        Response response = request(Method.GET, "https://restapi.wcaquino.me/ola");
         Assertions.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
         Assertions.assertTrue(response.getStatusCode() == 200);
         Assertions.assertTrue(response.getStatusCode() == 200, "O status code deveria ser 201");
-        Assertions.assertEquals(201, response.getStatusCode());
+        Assertions.assertEquals(200, response.getStatusCode());
 
         ValidatableResponse validate = response.then();
         validate.statusCode(200);
+    }
+
+    @Test
+    public void devoConhecerOutrasFormasDeTrabalharComRestAssured() {
+        Response response = request(Method.GET, "https://restapi.wcaquino.me/ola");
+        ValidatableResponse validate = response.then();
+        validate.statusCode(200);
+
+        get("https://restapi.wcaquino.me/ola").then().statusCode(201);
     }
 }
