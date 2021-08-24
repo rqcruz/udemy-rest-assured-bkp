6b9c15239cc351957ac22b509aa357655f024369
rqcruz <ronaldocruz183@gmail.com>
2021-08-23 21:49:45 -0300

    Criando teste usando assert do Hamcrest

--- src/test/java/br/com/rcruz/rest/OlaMundoTest.java
+++ src/test/java/br/com/rcruz/rest/OlaMundoTest.java
@@ -1,13 +1,18 @@
 package br.com.rcruz.rest;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
-import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class OlaMundoTest {
 
@@ -37,5 +42,27 @@ public class OlaMundoTest {
         given()
                 .when().get("https://restapi.wcaquino.me/ola")
                 .then().statusCode(200);
+    }
+
+    @Test
+    public void devoConhecerMatchersHamcrest() {
+        assertThat("Maria", is("Maria"));
+        assertThat(128, is(128));
+        assertThat(128, isA(Integer.class));
+        assertThat(128d, isA(Double.class));
+        assertThat(128d, greaterThan(125d));
+        assertThat(128d, lessThan(130d));
+
+        List<Integer> impares = Arrays.asList(1, 3, 5, 7, 9);
+        assertThat(impares, hasSize(5));
+        assertThat(impares, contains(1, 3, 5, 7, 9));
+        assertThat(impares, containsInAnyOrder(9, 5, 3, 7, 1));
+        assertThat(impares, hasItem(1));
+        assertThat(impares, hasItems(1, 9));
+
+        assertThat("Maria", is(not("Joao")));
+        assertThat("Maria", not("Joao"));
+        assertThat("Joaquina", anyOf(is("Maria"), is("Joaquina")));
+        assertThat("Joaquina", allOf(startsWith("Joa"), containsString("qui"), endsWith("ina")));
     }
 }
