5bc8b47950758af26c8e78da80209518925494cd
rqcruz <ronaldocruz183@gmail.com>
2021-08-24 21:16:11 -0300

    Adicionando teste que verifica mensagem de erro

--- src/test/java/br/com/rcruz/rest/UserJsonTest.java
+++ src/test/java/br/com/rcruz/rest/UserJsonTest.java
@@ -64,4 +64,14 @@ public class UserJsonTest {
                 .body("filhos.name", hasItems("Zezinho", "Luizinho"))
         ;
     }
+
+    @Test
+    public void deveRetornarErroUsuarioInexistente() {
+        given()
+                .when().get("https://restapi.wcaquino.me/users/4")
+                .then()
+                .statusCode(404)
+                .body("error", is("Usuário inexistente"))
+        ;
+    }
 }
